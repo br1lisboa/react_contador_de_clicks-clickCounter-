9519b1b1162069a49fb323438bfcbc43721da6b9
br1lisboa <brlisbo@gmail.com>
2022-06-24 21:16:23 -0300

    added component button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import fccLogo from './imagenes/logo-ffc.png';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('Clic');
+  };
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  };
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        className='freecodecamp-contenedor'
+        src={fccLogo}
+        alt='logo de freecodecamp'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+          texto={'Clic'}
+          esBotonDeClic={true}
+          manejarClic={manejarClic}/>
+        <Boton
+          texto={'Reiniciar'}
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
 }
